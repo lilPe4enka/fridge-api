fdcef10fa129c7a4b768d868618666cc876c4cd7
lilPe4enka <150793327+lilPe4enka@users.noreply.github.com>
2026-08-12 14:07:57 +0300

    Add files via upload

--- api.py
+++ api.py
@@ -57,7 +57,7 @@ async def find_recipes(request: IngredientsRequest):
     try:
         # Обращаемся к Gemini через новую библиотеку
         response = client.models.generate_content(
-            model='gemini-2.5-flash',
+            model='gemini-1.5-flash',
             contents=prompt,
             config=types.GenerateContentConfig(
                 response_mime_type="application/json",
