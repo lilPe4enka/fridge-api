f62aedc0ef80098e242802904e2567d129b190b7
lilPe4enka <150793327+lilPe4enka@users.noreply.github.com>
2026-08-12 22:06:02 +0300

    qwertapi.py

--- api.py
+++ api.py
@@ -66,7 +66,7 @@ async def find_recipes(request: IngredientsRequest):
     - "macros": объект с ключами "kcal", "protein", "fat", "carbs"
     """
 
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.6-flash:generateContent?key={GEMINI_API_KEY}"
     payload = {
         "contents": [{"parts": [{"text": prompt}]}],
         "generationConfig": {"responseMimeType": "application/json"}
